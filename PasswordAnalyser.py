6ba6135f72d7fcab5c0d1aceee9fe19911e44a59
eesha <eeshashaheen14@gmail.com>
2026-05-23 20:45:15 +0100

    Add current_password variable storing user input for the password they enter as their current. Print Analysing alongside their current password.

---
+++ PasswordAnalyser.py
@@ -0,0 +1,2 @@
+current_password = input("Please enter your current password: ") #Prompt user to input their current password
+print("Analysing: " + current_password) #Prints Analysing with the users current password
