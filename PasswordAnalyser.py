eefbcf0d7a607c8bcf501bfb6e5424546239cad7
eesha <eeshashaheen14@gmail.com>
2026-05-24 14:36:25 +0100

    Implement password scoring and input validation

--- PasswordAnalyser.py
+++ PasswordAnalyser.py
@@ -1,2 +1,12 @@
 current_password = input("Please enter your current password: ") #Prompt user to input their current password
-print("Analysing: " + current_password) #Prints Analysing with the users current password
+print("Analysing: " + current_password) #Print Analysing with the users current password
+
+score = 0 #Initialise password score to zero
+
+if len(current_password) < 6:
+    score = score + 0 #Award zero points for a password equal to or under 6 characters
+elif len(current_password) >= 6 and len(current_password) <= 9:
+    score = score + 1 #Award one point if length is between six and nine characters
+else:
+    score = score + 2 #Award two points if character length is equal to or over ten
+
