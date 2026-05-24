734852ef590e196d236469dc052ab99755fb47af
eesha <eeshashaheen14@gmail.com>
2026-05-24 14:53:05 +0100

    Implement password length scoring system

--- PasswordAnalyser.py
+++ PasswordAnalyser.py
@@ -1,12 +1,14 @@
-current_password = input("Please enter your current password: ") #Prompt user to input their current password
-print("Analysing: " + current_password) #Print Analysing with the users current password
+current_password = input("Please enter your current password: ") #Prompts user to input their current password
+print(f"Analysing: {current_password}") #Prints Analysing with the users current password
 
-score = 0 #Initialise password score to zero
+score = 0 #Initialises password score to zero
 
 if len(current_password) < 6:
-    score = score + 0 #Award zero points for a password equal to or under 6 characters
+    score = score + 0 #Awards zero points for a password equal to or under 6 characters
 elif len(current_password) >= 6 and len(current_password) <= 9:
-    score = score + 1 #Award one point if length is between six and nine characters
+    score = score + 1 #Awards one point if length is between six and nine characters
 else:
-    score = score + 2 #Award two points if character length is equal to or over ten
+    score = score + 2 #Awards two points if character length is equal to or over ten
+
+print(f"Your score is: {score}") #Prints password score
 
