d63f7617873adf6cb3d4c818d018f504761793cc
eesha <eeshashaheen14@gmail.com>
2026-05-25 22:27:54 +0100

    Connect HTML password input form to Flask backend using POST requests

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        current_password = request.form["password"]
+        print(current_password)
     return render_template("index.html")
 
 if __name__ == "__main__":
