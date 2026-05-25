fb5fc1ff0d879fdd4ce392ecd260884c81a22511
eesha <eeshashaheen14@gmail.com>
2026-05-25 11:13:40 +0100

    Implement password character (letter, number or symbol) detection using boolean logic and loops

--- PasswordAnalyser.py
+++ PasswordAnalyser.py
@@ -15,8 +15,31 @@ has_letter = False
 has_number = False
 has_symbol = False
 
+for character in current_password:
+    if character.isalpha(): #If alphabetical letter
+        has_letter = True
+    elif character.isdigit(): #If number
+        has_number = True
+    else: #Else symbol
+        has_symbol = True 
+
+if has_letter and has_number and has_symbol:
+    score = score + 3 
+elif has_letter and has_number:
+    score = score + 2
+elif has_letter and has_symbol:
+    score = score + 2
+elif has_symbol and has_number:
+    score = score + 2
+elif has_letter:
+    score = score + 1
+elif has_number:
+    score = score + 1
+elif has_symbol:
+    score = score + 1
+else:
+    print("Invalid password, please enter your password again. ") #Prints if user did not enter a value
 
 
-
-print(f"Your score is: {score}") #Prints password score
+print(f"Your password strength score is: {score}") #Prints password score
 
