a874ba9a7beffebcfe42e05dc64a6ef76e5e54ea
eesha <eeshashaheen14@gmail.com>
2026-05-26 16:50:44 +0100

    Integrate password length scoring into the Flask web application

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ def home():
     score = None
     if request.method == "POST":
         current_password = request.form["password"]
-        print(current_password)
+        score = 0
+        if len(current_password) < 6:
+            score = score + 0 #Awards zero points for a password equal to or under 6 characters
+        elif len(current_password) >= 6 and len(current_password) <= 9:
+            score = score + 1 #Awards one point if length is between six and nine characters
+        else:
+            score = score + 2 #Awards two points if character length is equal to or over ten
+ 
     return render_template("index.html", score=score)
 
 if __name__ == "__main__":
