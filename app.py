7f77a8465ecadccbee41c954a59be5dc52ee1ac4
eesha <eeshashaheen14@gmail.com>
2026-05-26 16:41:38 +0100

    Add password input form and connect it to the Flask backend

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    score = None
     if request.method == "POST":
         current_password = request.form["password"]
         print(current_password)
-    return render_template("index.html")
+    return render_template("index.html", score=score)
 
 if __name__ == "__main__":
     app.run(debug=True)
