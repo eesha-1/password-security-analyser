60167eaaba46ab8b2323807001860fe65a2b8920
eesha <eeshashaheen14@gmail.com>
2026-05-28 13:36:31 +0100

    Fix indentation error

--- app.py
+++ app.py
@@ -12,87 +12,91 @@ def home():
     error_message = None
     if request.method == "POST":
         current_password = request.form["password"]
+        wants_recommendation = request.form.get("recommendation")
         if current_password.strip() == "":
              error_message = "Invalid password. Please enter your password again."
-        score = 0
-        if len(current_password) < 6:
-            score = score + 0 #Awards zero points for a password equal to or under 6 characters
-        elif len(current_password) >= 6 and len(current_password) <= 9:
-            score = score + 1 #Awards one point if length is between six and nine characters
-        else:
-            score = score + 2 #Awards two points if character length is equal to or over ten
+        else:     
+             score = 0
+
+             if len(current_password) < 6:
+                  score = score + 0 #Awards zero points for a password equal to or under 6 characters
+             elif len(current_password) >= 6 and len(current_password) <= 9:
+                  score = score + 1 #Awards one point if length is between six and nine characters
+             else:
+                  score = score + 2 #Awards two points if character length is equal to or over ten
         
-        #Initially sets the password to contain no letters, numbers or symbols
-        has_letter = False
-        has_number = False
-        has_symbol = False
+             #Initially sets the password to contain no letters, numbers or symbols
+             has_letter = False
+             has_number = False
+             has_symbol = False
 
-        for character in current_password:
-            if character.isalpha(): #If alphabetical letter
-                has_letter = True
-            elif character.isdigit(): #If number
-                has_number = True
-            else: #Else symbol
-                has_symbol = True 
+             for character in current_password:
+                  if character.isalpha(): #If alphabetical letter
+                       has_letter = True
+                  elif character.isdigit(): #If number
+                       has_number = True
+                  else: #Else symbol
+                       has_symbol = True 
 
-        if has_letter and has_number and has_symbol:
-            score = score + 3 
-        elif has_letter and has_number:
-            score = score + 2
-        elif has_letter and has_symbol:
-            score = score + 2
-        elif has_symbol and has_number:
-            score = score + 2
-        elif has_letter:
-            score = score + 1
-        elif has_number:
-            score = score + 1
-        elif has_symbol:
-            score = score + 1
+             if has_letter and has_number and has_symbol:
+                  score = score + 3 
+             elif has_letter and has_number:
+                score = score + 2
+             elif has_letter and has_symbol:
+                score = score + 2
+             elif has_symbol and has_number:
+                score = score + 2
+             elif has_letter:
+                score = score + 1
+             elif has_number:
+                score = score + 1
+             elif has_symbol:
+                score = score + 1
 
-        #Checking lowercase and uppercase characters in the password
-        #initially sets the password to contain no lowercase or uppercase letters
-        has_lowercase = False 
-        has_uppercase = False
+             #Checking lowercase and uppercase characters in the password
+             #initially sets the password to contain no lowercase or uppercase letters
+             has_lowercase = False 
+             has_uppercase = False
 
-        for character in current_password:
-            if character.islower():
-                has_lowercase = True
-            elif character.isupper():
-                has_uppercase = True
-
-        if has_lowercase and has_uppercase: #If password contains mix of uppercase and lowercase letters
-            score = score + 2
-        elif has_lowercase:
-            score = score + 1
-        elif has_uppercase:
-            score = score + 1        
-        acceptable_password = False
-        while acceptable_password == False:
-            characters = string.ascii_letters + string.digits + string.punctuation
-            secure_password = "" #initially sets secure password to nothing
-            for i in range(12): #Generates 12 random characters
-                secure_password = secure_password + random.choice(characters)
-               #Initially sets generated password to have no upper or lowercase letters or numbers, symbols   
-            generated_has_letter = False
-            generated_has_number = False
-            generated_has_symbol = False
-            generated_has_uppercase = False
-            generated_has_lowercase = False
-
-            for character in secure_password:
-                if character.isalpha(): #If alphabetical letter
-                        generated_has_letter = True
-                if character.isdigit(): #If number
-                        generated_has_number = True
-                if not character.isalpha() and not character.isdigit():
-                        generated_has_symbol = True
-                if character.isupper():
-                        generated_has_uppercase = True
+             for character in current_password:
                 if character.islower():
-                        generated_has_lowercase = True 
-            if generated_has_letter and generated_has_number and generated_has_symbol and generated_has_uppercase and generated_has_lowercase:
-                    acceptable_password = True
+                    has_lowercase = True
+                elif character.isupper():
+                    has_uppercase = True
+
+             if has_lowercase and has_uppercase: #If password contains mix of uppercase and lowercase letters
+                score = score + 2
+             elif has_lowercase:
+                score = score + 1
+             elif has_uppercase:
+                score = score + 1     
+             if wants_recommendation:   
+                acceptable_password = False
+                while acceptable_password == False:
+                    characters = string.ascii_letters + string.digits + string.punctuation
+                    secure_password = "" #initially sets secure password to nothing
+                    for i in range(12): #Generates 12 random characters
+                        secure_password = secure_password + random.choice(characters)
+                    #Initially sets generated password to have no upper or lowercase letters or numbers, symbols   
+                    generated_has_letter = False
+                    generated_has_number = False
+                    generated_has_symbol = False
+                    generated_has_uppercase = False
+                    generated_has_lowercase = False
+
+                    for character in secure_password:
+                            if character.isalpha(): #If alphabetical letter
+                                generated_has_letter = True
+                            if character.isdigit(): #If number
+                                generated_has_number = True
+                            if not character.isalpha() and not character.isdigit():
+                                generated_has_symbol = True
+                            if character.isupper():
+                                generated_has_uppercase = True
+                            if character.islower():
+                                generated_has_lowercase = True 
+                    if generated_has_letter and generated_has_number and generated_has_symbol and generated_has_uppercase and generated_has_lowercase:
+                        acceptable_password = True
                 
     return render_template("index.html", score=score, secure_password=secure_password, error_message=error_message)
 
